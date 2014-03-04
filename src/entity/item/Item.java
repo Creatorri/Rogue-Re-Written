29d238f2305169e2b7924e37b8a0f36dbbd44992
Creatorri <torridon@live.com>
2014-03-03 19:25:35 -0500

    Many much edit

--- src/entity/item/Item.java
+++ src/entity/item/Item.java
@@ -8,55 +8,18 @@ import entity.Entity;
  * Items
  * @author Torri
  */
-public class Item extends Entity{
-    @Override
-    public void turn() {
-    }
-    public enum materials{
-        Wood,Stone,Copper,Bronze,Silver,Gold,Iron,Steel,Unknown
-    }
-    public enum type{
-        Empty,Sword,Axe,Shield,Ring
-    }
-    public enum modifiers{
-        NOMODIFIER,Broken,Ordinary,Shattered,Old,Healthy,Magical,Strong
+public abstract class Item extends Entity{
+    /**
+     * creates item
+     * @param l 
+     */
+    public Item(Level l) {
+        super(l);
     }
     /**
      * parent object
      */
     public Entity parent;
-    /**
-     * Creates Item
-     * @param l
-     * @param parent
-     * @param id 
-     */
-    public Item(Level l,Entity parent,int id){
-        super(l);
-        int lvl = rand.nextInt(Level.NUMLEVELS+3)+1;
-        this.parent=parent;
-        if(id==0) name=type.Empty.name();
-        name="Lvl "+lvl+" "+modifiers.values()[id%modifiers.values().length].name()+" "+materials.values()[id%materials.values().length].name()+" "+type.values()[(id%(type.values().length-1))+1];
-        switch(materials.values()[id%materials.values().length]){
-            
-        }
-        switch(type.values()[id%type.values().length]){
-            
-        }
-        switch(modifiers.values()[id%modifiers.values().length]){
-            
-        }
-    }
-    /**
-     * Creates Item
-     * @param l
-     * @param parent
-     * @param id 
-     */
-    public Item(Level l,Entity parent,String id){
-        super(l);
-        this.parent=parent;
-    }
     /**
      * Puts item on the floor
      */
